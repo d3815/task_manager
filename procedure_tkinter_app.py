dc3ad6a85227882606e1f0f10affc20d4b5ad8d3
zolotarev <denis3olotarev@mail.ru>
2020-05-17 20:28:52 +0300

    tech: today tasks were added

--- procedure_tkinter_app.py
+++ procedure_tkinter_app.py
@@ -3,7 +3,46 @@
 
 from tkinter import *
 from tkinter import messagebox
+import datetime
 import sqlite3
+
+
+def update_today_list_frame():
+    for widget in today_task_frame.winfo_children():
+        widget.destroy()
+
+    today_task_list = show_today_task()
+    task_count = Label(today_task_frame, text='Всего задач : {}'.format(len(today_task_list)))
+    task_count.pack(side=TOP)
+
+    task_canvas = Canvas(today_task_frame)
+    task_list_scrollbar = Scrollbar(today_task_frame, orient="vertical", command=task_canvas.yview)
+    today_tasks_frame = Frame()
+
+    for i in today_task_list:
+        lab_title = Label(today_tasks_frame, text=str.upper(i[1]))
+        lab_title.pack()
+        lab_desc = Label(today_tasks_frame, text=str.capitalize(i[2]))
+        lab_desc.pack()
+        lab_date = Label(today_tasks_frame, text=i[3])
+        lab_date.pack()
+        lab_priority = Label(today_tasks_frame, text='Приоритет {}'.format(i[4]))
+        lab_priority.pack()
+        del_button = Button(today_tasks_frame, text='Удалить задачу', command=lambda i=i: delete_task(i[0]))
+        del_button.pack()
+
+    task_canvas.create_window(0, 0, anchor='nw', window=today_tasks_frame)
+    task_canvas.update_idletasks()
+    task_canvas.configure(scrollregion=task_canvas.bbox('all'), yscrollcommand=task_list_scrollbar.set)
+    task_canvas.pack(fill='both', expand=True, side='left')
+
+    task_list_scrollbar.pack(side=RIGHT, fill=Y)
+
+
+
+
+    context = show_today_task()
+    return context
 
 
 def delete_task(task_id):
@@ -14,16 +53,32 @@ def delete_task(task_id):
     conn.commit()
     conn.close()
     update_task_list_frame()
+    update_today_list_frame()
 
 
-def show_task():
+def show_today_task():
+    time_today = datetime.datetime.today()
+    conn = sqlite3.connect('mydatabase.db')
+    cur = conn.cursor()
+    sql_create = "CREATE TABLE IF NOT EXISTS Tasks (task_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, " \
+                 "description TEXT, due TEXT, priority INTEGER)"
+    cur.execute(sql_create)
+    sql_show_today_task = "Select * FROM Tasks WHERE due = '{}-{}-{}'".format(time_today.day, time_today.month,
+                            str(time_today.year)[-2:])
+    cur.execute(sql_show_today_task)
+    context = cur.fetchall()
+    conn.close()
+    return context
+
+
+def show_all_task():
     conn = sqlite3.connect("mydatabase.db")
     cur = conn.cursor()
-    sql_create = 'CREATE TABLE IF NOT EXISTS Tasks (task_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, ' \
-                 'description TEXT, due TEXT, priority INTEGER)'
+    sql_create = "CREATE TABLE IF NOT EXISTS Tasks (task_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, " \
+                 "description TEXT, due TEXT, priority INTEGER)"
     cur.execute(sql_create)
-    sql_show_info = 'SELECT * FROM Tasks ORDER BY due, priority'
-    cur.execute(sql_show_info)
+    sql_show_all_tasks = 'SELECT * FROM Tasks ORDER BY due, priority'
+    cur.execute(sql_show_all_tasks)
     context = cur.fetchall()
     conn.close()
     return context
@@ -56,10 +111,9 @@ def add_task():
         cursor.execute(sql_update)
         conn.commit()
         conn.close()
-        # out_task_name = f'Задача {loc_title} успешно добавлена'
-        # messagebox.showinfo('Success', out_task_name)
         delete_window_context()
         update_task_list_frame()
+        update_today_list_frame()
         title.focus()
     else:
         messagebox.showinfo('Failed', 'Введены некорректные данные')
@@ -69,7 +123,7 @@ def update_task_list_frame():
     for widget in all_task_frame.winfo_children():
         widget.destroy()
 
-    task_list = show_task()
+    task_list = show_all_task()
     task_count = Label(all_task_frame, text='Всего задач : {}'.format(len(task_list)))
     task_count.pack(side=TOP)
 
@@ -78,7 +132,6 @@ def update_task_list_frame():
     frame = Frame()
 
     for i in task_list:
-        # current_task = Frame(task_canvas)
         lab_title = Label(frame, text=str.upper(i[1]))
         lab_title.pack()
         lab_desc = Label(frame, text=str.capitalize(i[2]))
@@ -90,7 +143,6 @@ def update_task_list_frame():
         del_button = Button(frame, text='Удалить задачу', command=lambda i=i: delete_task(i[0]))
         del_button.pack()
 
-        # current_task.pack()
     task_canvas.create_window(0, 0, anchor='nw', window=frame)
     task_canvas.update_idletasks()
     task_canvas.configure(scrollregion=task_canvas.bbox('all'), yscrollcommand=task_list_scrollbar.set)
@@ -99,33 +151,12 @@ def update_task_list_frame():
     task_list_scrollbar.pack(side=RIGHT, fill=Y)
 
 
-'''
-
-canvas = tk.Canvas(parent)
-scroll_y = tk.Scrollbar(parent, orient="vertical", command=canvas.yview)
-
-frame = tk.Frame(canvas)
-# group of widgets
-for i in range(20):
-    tk.Label(frame, text='label %i' % i).pack()
-# put the frame in the canvas
-canvas.create_window(0, 0, anchor='nw', window=frame)
-# make sure everything is displayed before configuring the scrollregion
-canvas.update_idletasks()
-
-canvas.configure(scrollregion=canvas.bbox('all'), yscrollcommand=scroll_y.set)
-                 
-canvas.pack(fill='both', expand=True, side='left')
-scroll_y.pack(fill='y', side='right')
-
-'''
-
 root = Tk()
 
 root.title('Task Manager')
 w = root.winfo_screenwidth() // 2
 h = root.winfo_screenheight() // 2
-root.geometry('800x300+{}+{}'.format(w, h))
+root.geometry('800x280+{}+{}'.format(w, h))
 root.resizable(width=False, height=False)
 
 new_task_frame = Frame(root)
@@ -161,11 +192,11 @@ priority.pack(side=TOP)
 btn = Button(new_task_frame, text="Добавить", command=add_task)
 btn.pack(side=TOP)
 
-
 new_task_frame.pack(side=LEFT, ipadx=10)
 all_task_frame.pack(side=LEFT, ipadx=10)
 today_task_frame.pack(side=LEFT, ipadx=10)
 
 update_task_list_frame()
+update_today_list_frame()
 
 root.mainloop()
